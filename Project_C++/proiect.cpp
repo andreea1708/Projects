e61307295355653ee7a170e22a0c5bbf11116e40
andreea1708 <100288931+andreea1708@users.noreply.github.com>
2022-07-02 17:46:13 +0300

    Update proiect.cpp

--- Project_C++/proiect.cpp
+++ Project_C++/proiect.cpp
@@ -6,17 +6,17 @@ using namespace std;
 int main()
 {
     cout<< "--------------------------"<<endl;
-    cout<< "1 --- Crearea unui vector;"<<endl;
-    cout<< "2 --- Inversarea elementelor unui vector;"<<endl;
-    cout<< "3 --- Adauga un element la sfarsitul unui vector;"<<endl;
-    cout<< "4 --- Elimina ultimul element din vector;"<<endl;
-    cout<< "5 --- Schimba continutul;"<<endl;
-    cout<< "6 --- Insereaza in vector;"<<endl;
-    cout<< "7 --- Sterge un element din vector;"<<endl;
-    cout<< "8 --- Sterge vectorul;"<<endl;
-    cout<< "9 --- Verific daca am elemente in vector;"<<endl;
-    cout<< "10--- Verific cate elemente are vectorul;"<<endl;
-    cout<< "0 --- Iesire;"<<endl;
+    cout<< "1 --- Creating a vector;"<<endl;
+    cout<< "2 --- Inverting the elements of a vector;"<<endl;
+    cout<< "3 --- Add an element to the end of a vector;"<<endl;
+    cout<< "4 --- Remove the last element from the vector;"<<endl;
+    cout<< "5 --- Change the content;"<<endl;
+    cout<< "6 --- Insert in vector;"<<endl;
+    cout<< "7 --- Delete an element from the vector;"<<endl;
+    cout<< "8 --- Delete the vector;"<<endl;
+    cout<< "9 --- I check if I have elements in the vector;"<<endl;
+    cout<< "10--- I check how many elements the vector has;"<<endl;
+    cout<< "0 --- Exit;"<<endl;
     cout<< "--------------------------"<<endl;
     vector<int>myVector;
     vector<int>::iterator it;
@@ -27,16 +27,16 @@ int main()
         if(n==1)
        {
         int dim, elem;
-        cout<<"Alege dimensiunea vectorului: ";
+        cout<<"Choose the size of the vector: ";
         cin>>dim;
-        cout<<"Introdu elementele vectorului: ";
+        cout<<"Enter vector elements: ";
         for(i=0;i<dim;i++)
         {
           cin>>elem;
           myVector.push_back(elem);
         }
         cout<<endl;
-        cout << "Acesta este vectorul nostru: ";
+        cout << "This is our vector: ";
         for(it=myVector.begin();it!=myVector.end();it++)
                    cout<<*it<<" ";
         cout <<endl;
@@ -45,11 +45,11 @@ int main()
           if(n==2)
           {
             if(myVector.empty())
-               cout<<"Nu exista elemente in vector. Intai creeaza un vector! ";
+               cout<<"There are no elements in the vector. First create a vector! ";
             else
                {
                  reverse(myVector.begin(),myVector.end());
-                 cout<<"Vectorul dupa inversare: "<<endl;
+                 cout<<"Vector after inversion: "<<endl;
                  for(it=myVector.begin();it!=myVector.end();it++)
                             cout<<*it<<" ";
                  cout <<endl;
@@ -61,13 +61,13 @@ int main()
               {
                 int nr;
                 if(myVector.empty())
-                      cout<<"Nu exista elemente in vector. Intai creeaza un vector! ";
+                      cout<<"There are no elements in the vector. First create a vector! ";
                else
                 {
-                  cout<<"Elementul pe care il adaugi:";
+                  cout<<"The item you are adding:";
                   cin>>nr;
                   myVector.push_back(nr);
-                  cout<<"Vectorul dupa adaugare: "<<endl;
+                  cout<<"Vector after addition: "<<endl;
                   for(it=myVector.begin();it!=myVector.end();it++)
                             cout<<*it<<" ";
                   cout <<endl;
@@ -77,11 +77,11 @@ int main()
                  if(n==4)
                  {
                    if(myVector.empty())
-                         cout<<"Nu exista elemente in vector. Intai creeaza un vector! "<<endl;
+                         cout<<"There are no elements in the vector. First create a vector! "<<endl;
                    else
                    {
                      myVector.pop_back();
-                  cout<<"Vectorul dupa eliminarea ultimului element: "<<endl;
+                  cout<<"Vector after removing the last element: "<<endl;
                   for(it=myVector.begin();it!=myVector.end();it++)
                             cout<<*it<<" ";
                   cout <<endl;
@@ -91,43 +91,43 @@ int main()
                     if(n==5)
                     {
                       if(myVector.empty())
-                            cout<<"Nu exista elemente in vector. Intai creeaza un vector! ";
+                            cout<<"There are no elements in the vector. First create a vector! ";
                       else
                       {
                         vector<int>Vector;
                         vector<int>::iterator ite;
-                        cout<<"Al doilea vector: "<<endl;
+                        cout<<"Second vector: "<<endl;
                         int dimen, element;
-                        cout<<"Alege dimensiunea vectorului: ";
+                        cout<<"Choose the size of the vector: ";
                         cin>>dimen;
-                        cout<<"Introdu elementele vectorului: ";
+                        cout<<"Enter vector elements: ";
                         for(i=0;i<dimen;i++)
                         {
                             cin>>element;
                             Vector.push_back(element);
                         }
                         cout<<endl;
-                        cout << "Acesta este al doilea vector al nostru: ";
+                        cout << "This is our second vector: ";
                         for(ite=Vector.begin();ite!=Vector.end();ite++)
                                  cout<<*ite<<" ";
                         cout <<endl;
                         cout <<endl;
 
-                        cout << "Acesta este primul vector al nostru: ";
+                        cout << "This is our first vector: ";
                         for(it=myVector.begin();it!=myVector.end();it++)
                                  cout<<*it<<" ";
                         cout <<endl;
                         cout <<endl;
 
                         Vector.swap(myVector);
-                        cout<<"Dupa schimbarea continutului:"<<endl;
+                        cout<<"After changing the content:"<<endl;
                         cout <<endl;
-                        cout << "Acesta este primul vector al nostru: ";
+                        cout << "This is our first vector: ";
                         for(it=myVector.begin();it!=myVector.end();it++)
                                  cout<<*it<<" ";
                         cout <<endl;
                         cout <<endl;
-                        cout << "Acesta este al doilea vector al nostru: ";
+                        cout << "This is our second vector: ";
                         for(ite=Vector.begin();ite!=Vector.end();ite++)
                                  cout<<*ite<<" ";
                         cout <<endl;
@@ -139,32 +139,32 @@ int main()
                         if(n==6)
                         {
                           if(myVector.empty())
-                                cout<<"Nu exista elemente in vector. Intai creeaza un vector! ";
+                                cout<<"There are no elements in the vector. First create a vector! ";
                           else
                           {
-                            cout<<"Creeaza un vector:"<<endl;
+                            cout<<"Create a vector:"<<endl;
                             vector<int>anothervector;
                             vector<int>::iterator y;
                             int di, el;
-                        cout<<"Alege dimensiunea vectorului: ";
+                        cout<<"Choose the size of the vector: ";
                         cin>>di;
-                        cout<<"Introdu elementele vectorului: ";
+                        cout<<"Enter vector elements: ";
                         for(i=0;i<di;i++)
                         {
                             cin>>el;
                             anothervector.push_back(el);
                         }
                         cout<<endl;
-                        cout<<"Acesta este al doilea vector al nostru: ";
+                        cout<<"This is our second vector: ";
                         for(y=anothervector.begin();y!=anothervector.end();y++)
                                  cout<<*y<<" ";
                         cout <<endl;
-                        cout<<"Alege din ce pozitie sa se insereze: ";
+                        cout<<"Choose from which position to insert: ";
                         int m;
                         cin>>m;
                         y=myVector.begin();
                         myVector.insert(y+m,anothervector.begin(),anothervector.end());
-                        cout<<"Vectorul nostru dupa insert: ";
+                        cout<<"Our vector by insert: ";
                         for(it=myVector.begin();it!=myVector.end();it++)
                                    cout<<*it<<" ";
                         cout <<endl;
@@ -176,16 +176,16 @@ int main()
                           if(n==7)
                           {
                             if(myVector.empty())
-                                cout<<"Nu exista elemente in vector. Intai creeaza un vector! ";
+                                cout<<"There are no elements in the vector. First create a vector! ";
                             else
                             {
-                              cout<<"Alege care element sa fie sters: "<<endl;
-                              cout<<"pozitia=";
+                              cout<<"Choose which item to delete: "<<endl;
+                              cout<<"position =";
                               int pozitie;
                               cin>>pozitie;
                               myVector.erase(myVector.begin()+pozitie);
                               cout <<endl;
-                              cout<<"Noul vector dupa stergere: ";
+                              cout<<"New vector after deletion: ";
                               for(it=myVector.begin();it!=myVector.end();it++)
                                       cout<<*it<<" ";
                               cout <<endl;
@@ -197,20 +197,20 @@ int main()
                             if(n==8)
                             {
                               if(myVector.empty())
-                                    cout<<"Nu exista elemente in vector. Intai creeaza un vector! ";
+                                    cout<<"There are no elements in the vector. First create a vector! ";
                               else
                                   {
                                     myVector.clear();
-                                    cout<<"S-a sters vectorul";
+                                    cout<<"The vector has been deleted";
                                   }
                             }
                             else
                                 if(n==9)
                                 {
                                   if(myVector.empty())
-                                        cout<<"Nu exista elemente in vector.";
+                                        cout<<"There are no elements in the vector.";
                                   else
-                                        cout<<"Exista elemente în vector.";
+                                        cout<<"There are elements in the vector.";
                                 }
                                 else
                                    if(n==0)
@@ -220,33 +220,33 @@ int main()
                                       if(n==10)
                                          {
                                            if(myVector.empty())
-                                                cout<<"Nu exista elemente in vector. Intai creeaza un vector! ";
+                                                cout<<"There are no elements in the vector. First create a vector! ";
                                            else
                                            {
-                                              cout<<"Numarul de elemente din vector: "<<myVector.size();
+                                              cout<<"Number of elements in the vector: "<<myVector.size();
                                               cout<<endl;
                                            }
                                          }
                                     }
                                        if(n<0||n>10)
-                                             cout<<"Eroare! Selecteaza un numar din meniu!";
+                                             cout<<"Error! Select a number from the menu!";
 
                           }
                         }
 
       cout<<endl;
       cout<< "--------------------------"<<endl;
-      cout <<"1 --- Crearea unui vector;"<<endl;
-      cout<< "2 --- Inversarea elementelor unui vector;"<<endl;
-      cout<< "3 --- Adauga un element la sfarsitul unui vector;"<<endl;
-      cout<< "4 --- Elimina ultimul element din vector;"<<endl;
-      cout<< "5 --- Schimba continutul;"<<endl;
-      cout<< "6 --- Insereaza in vector;"<<endl;
-      cout<< "7 --- Sterge un element din vector;"<<endl;
-      cout<< "8 --- Sterge vectorul;"<<endl;
-      cout<< "9 --- Verific daca am elemente in vector;"<<endl;
-      cout<< "10--- Verific cate elemente are vectorul;"<<endl;
-      cout<< "0 --- Iesire;"<<endl;
+      cout <<"1 --- Creating a vector;"<<endl;
+      cout<< "2 --- Inverting the elements of a vector;"<<endl;
+      cout<< "3 --- Add an element to the end of a vector;"<<endl;
+      cout<< "4 --- Remove the last element from the vector;"<<endl;
+      cout<< "5 --- Change the content;"<<endl;
+      cout<< "6 --- Insert in vector;"<<endl;
+      cout<< "7 --- Delete an element from the vector;"<<endl;
+      cout<< "8 --- Delete the vector;"<<endl;
+      cout<< "9 --- I check if I have elements in the vector;"<<endl;
+      cout<< "10--- I check how many elements the vector has;"<<endl;
+      cout<< "0 --- Exit;"<<endl;
       cout<< "--------------------------"<<endl;
       cin>>n;
     }
